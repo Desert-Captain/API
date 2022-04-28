157a3837db32f96fbea9bfe4e9b7379ef9791ad4
Bennett Heyn <heynbennett1@gmail.com>
2022-04-28 13:20:14 -0400

    finished before securing the code

--- Desert.Captain.Api/Security/HasScopeHandler.cs
+++ Desert.Captain.Api/Security/HasScopeHandler.cs
@@ -12,7 +12,9 @@ namespace Desert.Captain.Api.Security
 			if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
 				return Task.CompletedTask;
 
-			var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+			var scopes = context.User
+			.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer)
+			.Value.Split(' ');
 
 			if (scopes.Any(s => s == requirement.Scope))
 				context.Succeed(requirement);
